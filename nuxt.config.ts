770a9a9adf54915e7f59018c79ce34fcbe37033d
Faustino Hokama <fhokama@equoplatform.com>
2021-12-10 22:48:12 -0300

    Add typescript and vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
-import { defineNuxtConfig } from '@nuxt/bridge'
+import { defineNuxtConfig } from "@nuxt/bridge";
 
 export default defineNuxtConfig({
-})
+  bridge: {
+    vite: true,
+  },
+  vite: {
+    build: {
+      sourcemap: true,
+    },
+  },
+});
